90eedff2ce93c5549d7ce26c077f91ba6881e9ef
Mac Thu <thumt189utc@gmail.com>
2018-01-06 13:22:57 +0700

    Screen play

---
+++ Assets/Scipts/Controller Scripts/LevelMenuCtrlScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMenuCtrlScript : MonoBehaviour {
+
+	public void Level()
+    {
+        Application.LoadLevel(name: "GamePlayer");
+    }
+
+    public void BackMenu()
+    {
+        Application.LoadLevel(name: "MainMenu");
+    }
+}
